c6f6bf076a086d93b94733343741677f35d30407
Rabbit <labcgood44@gmail.com>
2024-01-05 16:34:47 +0800

    Initial Commit

---
+++ cheersClicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cheersClicker
+//
+//  Created by Labe on 2024/1/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
